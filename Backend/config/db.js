5049279ab08a6bc8f2ad47d795d1f596e7cd2546
Tayyaba Akhter <tayyabaakhter189@gmail.com>
2026-04-23 21:32:42 +0500

    ..

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,8 +6,12 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'herbarium_db',
+  port: process.env.DB_PORT || 4000, // <--- Add this line
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: true 
+  }
 });
 
 module.exports = pool.promise(); // Using promises makes code cleaner
