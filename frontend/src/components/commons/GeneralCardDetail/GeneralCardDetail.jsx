23df365fae622701ca5cfbc30c590097f9cd3fac
Tayyaba Akhter <tayyabaakhter189@gmail.com>
2026-04-28 12:01:08 +0500

    ..

--- frontend/src/components/commons/GeneralCardDetail/GeneralCardDetail.jsx
+++ frontend/src/components/commons/GeneralCardDetail/GeneralCardDetail.jsx
@@ -10,7 +10,7 @@ const GeneralCardDetail = ({ title, icon, fields }) => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-y-6 gap-x-12">
         {fields.map((field, index) => (
           <div key={index} className={field.fullWidth ? "col-span-full" : ""}>
-            <p className="text-[12px] font-bold uppercase tracking-wider  mb-1 text-gray-9..00">
+            <p className="text-[12px] font-bold uppercase tracking-wider  mb-1 text-amber-300">
               {field.label}
             </p>
             <div className="text-base font-medium text-slate-100 dark:text-white">
