78907f1074c3ec5e74cb4e481c3846f06460cc2d
Tayyaba Akhter <tayyabaakhter189@gmail.com>
2026-06-23 22:52:54 +0500

    ..

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,8 +3,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://fyp26-digital-herbarium.onrender.com/axiosInstance/plants",
-    // baseURL: import.meta.env.VITE_API_URL || "https://fyp26-digital-herbarium.onrender.com/api",
+    // baseURL: import.meta.env.VITE_API_URL || "https://fyp26-digital-herbarium.onrender.com/axiosInstance/plants",
+    baseURL: import.meta.env.VITE_API_URL || "https://fyp26-digital-herbarium.onrender.com/api",
     //  baseURL: "http://localhost:4000/api" ,
      headers: { 'Content-Type': 'application/json' },
 });
